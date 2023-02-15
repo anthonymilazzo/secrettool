eb1732fbee526aba2bbb1b6f5f0a3ed03434b66b
BuildTools <ajmilazzo@gmail.com>
2023-02-14 21:02:35 -0500

    multiple envs

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ const filename = "secrets.yaml";
 const prompt = promptSync({ sigint: true });
 
 function renderSecretPath(
-  secretsConfig: { env: string; project: string; secrets: [string] },
-  secretName: string
+  secretsConfig: { envs: [string]; project: string; secrets: [string] },
+  secretName: string,
+  env: string
 ): string {
-  return `/${secretsConfig.env}/${secretsConfig.project}/${secretName}`;
+  return `/${env}/${secretsConfig.project}/${secretName}`;
 }
 
 function loadSecretsConfig(filename: string): any {
@@ -20,88 +21,118 @@ function loadSecretsConfig(filename: string): any {
 }
 
 function displayMeta(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
 }): void {
   console.log(`Project: ${secretsConfig.project}`);
-  console.log(`Env: ${secretsConfig.env}`);
+  console.log(`Envs: ${secretsConfig.envs}`);
   console.log("");
 }
 
 function displaySecrets(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
-}): void {
+}, env: string): void {
   console.log("Your configured 🤫 secrets:");
   secretsConfig.secrets.forEach((secret: string) => {
-    const secretPath = renderSecretPath(secretsConfig, secret);
+    const secretPath = renderSecretPath(secretsConfig, secret, env);
     console.log(` - ${secret} (${secretPath})`);
   });
   console.log("");
 }
 
+function displayEnvs(secretsConfig: {
+  envs: [string];
+  project: string;
+  secrets: [string];
+}): void {
+  secretsConfig.envs.forEach((env: string) => {
+    console.log(`Env: ${env}`);
+    displaySecrets(secretsConfig, env);
+  });
+  console.log("");
+}
+
 function secretExists(
-  secretsConfig: { env: string; project: string; secrets: [string] },
-  secret: string
+  secretsConfig: { envs: [string]; project: string; secrets: [string] },
+  secret: string,
+  env: string
 ): void {
-  const secretPath = renderSecretPath(secretsConfig, secret);
+  const secretPath = renderSecretPath(secretsConfig, secret, env);
   const exists = secretsManager.getSecret(secretPath);
   if (exists) {
-    console.log(` >>> Checking if \"${secretPath}\" exists... ✅`);
+    console.log(` >>> Checking if \"${secret}\" exists in \"${env}\"... ✅`);
   } else {
-    console.log(` >>> Checking if \"${secretPath}\" exists... X`);
+    console.log(` >>> Checking if \"${secret}\" exists in \"${env}\"... X`);
   }
 }
 
 function hasAccess(
-  secretsConfig: { env: string; project: string; secrets: [string] },
-  secret: string
+  secretsConfig: { envs: [string]; project: string; secrets: [string] },
+  secret: string,
+  env: string
 ): void {
-  const secretPath = renderSecretPath(secretsConfig, secret);
-  console.log(` >>> Checking if you have access to \"${secretPath}\"... ✅`);
+  console.log(` >>> Checking if you have access to \"${secret}\" in \"${env}\"... ✅`);
 }
 
-function checkAllSecrets(secretsConfig: {
-  env: string;
+function checkAllEnvs(secretsConfig: {
+  envs: [string];
   project: string;
   secrets: [string];
 }): void {
+  secretsConfig.envs.forEach((env: string) => {
+    checkAllSecrets(secretsConfig, env)
+  });
+}
+
+function checkAllSecrets(secretsConfig: {
+  envs: [string];
+  project: string;
+  secrets: [string];
+}, env: string): void {
   secretsConfig.secrets.forEach((secret: string) => {
-    console.log(` > Checking secret \"${secret}\"...`);
-    secretExists(secretsConfig, secret);
-    hasAccess(secretsConfig, secret);
-    console.log(`Secret \"${secret}\" is ready for use 🚀
+    console.log(` > Checking secret \"${secret}\" in \"${env}\"...`);
+    secretExists(secretsConfig, secret, env);
+    hasAccess(secretsConfig, secret, env);
+    console.log(`Secret \"${secret}\" in \"${env}\" is ready for use 🚀
 `);
   });
 }
 
 function chooseSecret(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
-}): string {
+}): { secret: string, env: string } {
   console.log(`Which one would you like to change?`);
   secretsConfig.secrets.forEach((secret: string) =>
     console.log(` > ${secret}`)
   );
   let secret = prompt("?> ");
-  return secret;
+
+  console.log(`Which env?`);
+  secretsConfig.envs.forEach((env: string) =>
+    console.log(` > ${env}`)
+  );
+  let env = prompt("?> ");
+  return { secret, env }
 }
 
 function changeSecret(
-  secretsConfig: { env: string; project: string; secrets: [string] },
-  secret: string
+  secretsConfig: { envs: [string]; project: string; secrets: [string] },
+  secret: string,
+  env: string
 ): void {
-  const secretPath = renderSecretPath(secretsConfig, secret);
+  const secretPath = renderSecretPath(secretsConfig, secret, env);
   let value = prompt("New value > ");
   secretsManager.updateSecret(secretPath, value);
   console.log(`Setting secret \"${secretPath}\"... ✅`);
 }
 
 function changeSecretLoop(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
 }): void {
@@ -113,15 +144,15 @@ function changeSecretLoop(secretsConfig: {
 }
 
 function setAnyValues(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
 }): boolean {
   let guess = prompt("Would you like to change any secret values? (y/N): ");
   console.log("");
   if (guess == "y") {
-    const secret = chooseSecret(secretsConfig);
-    changeSecret(secretsConfig, secret);
+    const { secret, env } = chooseSecret(secretsConfig);
+    changeSecret(secretsConfig, secret, env);
     return true;
   } else {
     return false;
@@ -133,18 +164,29 @@ function chooseSecretPath(): string {
   return secret;
 }
 
+function createSecretEnvs(secretsConfig: {
+  envs: [string];
+  project: string;
+  secrets: [string];
+}, secret: string): void {
+  secretsConfig.envs.forEach((env: string) => {
+    createSecret(secretsConfig, secret, env)
+  });
+}
+
 function createSecret(
-  secretsConfig: { env: string; project: string; secrets: [string] },
-  secret: string
+  secretsConfig: { envs: [string]; project: string; secrets: [string] },
+  secret: string,
+  env: string
 ): void {
   let value = prompt("Value > ");
-  const secretPath = renderSecretPath(secretsConfig, secret);
+  const secretPath = renderSecretPath(secretsConfig, secret, env);
   secretsManager.createSecret(secretPath, value);
   console.log(`Creating secret \"${secret}\"... ✅`);
 }
 
 function createSecretLoop(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
 }): void {
@@ -156,7 +198,7 @@ function createSecretLoop(secretsConfig: {
 }
 
 function createAnySecrets(secretsConfig: {
-  env: string;
+  envs: [string];
   project: string;
   secrets: [string];
 }): boolean {
@@ -164,7 +206,7 @@ function createAnySecrets(secretsConfig: {
   console.log("");
   if (guess == "y") {
     let secret = chooseSecretPath();
-    createSecret(secretsConfig, secret);
+    createSecretEnvs(secretsConfig, secret);
     return true;
   } else {
     return false;
@@ -175,14 +217,14 @@ function main(): void {
   try {
     console.log("== secrettool.io v1.0.0 [secrets.yaml] ==\n");
 
-    const secretsConfig: { env: string; project: string; secrets: [string] } =
+    const secretsConfig: { envs: [string]; project: string; secrets: [string] } =
       loadSecretsConfig(filename);
 
     displayMeta(secretsConfig);
 
-    displaySecrets(secretsConfig);
+    displayEnvs(secretsConfig);
 
-    checkAllSecrets(secretsConfig);
+    checkAllEnvs(secretsConfig);
 
     changeSecretLoop(secretsConfig);
 
